05c30b4f13879502c829e463d59fcc5d286358da
annandeborah <annandeborah@gmail.com>
2026-03-25 12:50:31 +0000

    style

---
+++ src/script.js
@@ -0,0 +1,34 @@
+let name = "Deborah";
+console.log(name);
+
+let age = 15;
+console.log(age);
+
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+console.log(sum);
+
+let difference = num1 - num2;
+console.log(difference);
+
+let product = num1 * num2;
+console.log(product);
+
+let quotient = num1 / num2;
+console.log(quotient);
+
+15 % 4; 
+let remainder = 15 % 4;
+console.log(remainder);
+
+let a = true;
+console.log(a);
+
+let b = 5;
+typeof b; 
+console.log(typeof b);
+    
+let c = "Hello";
+typeof c; 
+console.log(typeof c);
